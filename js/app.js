61322031e61c451e19793e5a75edec9b1a050f3f
Lana Kirilenko <svetlana.kirilenko@hotmail.com>
2019-07-12 16:10:17 +0100

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,97 @@
+const gameboard = document.querySelector('.gameboard')
+const menu = document.querySelector('.popupMenu')
+let currentIndex = 300
+const width = 18
+const scoreCounter = document.querySelector('.score')
+
+let score = 0
+
+
+// creating board
+for (let i = 0; i<324; i++) {
+  const tile = document.createElement('div')
+  tile.classList.add('tile')
+  gameboard.appendChild(tile)
+}
+const tiles = document.querySelectorAll('.tile')
+
+
+
+// level design
+const levelOne = [
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+  1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1,
+  1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1,
+  1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+  1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1,
+  1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1,
+  1, 1, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1,
+  0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+  1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1,
+  1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1,
+  1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1,
+  1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1,
+  1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 1,
+  1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1,
+  1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1,
+  1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+]
+
+// assigning walls and pacman
+for (let i = 0; i<tiles.length; i++) {
+  let item = levelOne[i]
+  if (item === 1) {
+    tiles[i].classList.add('wall')
+  }
+}
+
+//move pacman
+function pacMove(e) {
+  tiles[currentIndex].classList.remove('pacman')
+  switch(e.keyCode) {
+    case 37:
+      if (levelOne[currentIndex-1] !== 1) currentIndex -= 1
+      break
+    case 39:
+      if (levelOne[currentIndex+1] !== 1) currentIndex += 1
+      break
+    case 38:
+      if (levelOne[currentIndex-width] !== 1) currentIndex -= width
+      break
+    case 40:
+      if (levelOne[currentIndex+width] !== 1) currentIndex += width
+      break
+
+  }
+  // pacman eating dots
+  if(tiles[currentIndex].classList.contains('dot')) {
+    tiles[currentIndex].classList.remove('dot')
+    score++
+    scoreCounter.innerHTML = score
+
+  }
+  tiles[currentIndex].classList.add('pacman')
+
+}
+
+
+// set pacman food
+function setDots () {
+  let randomIndex = Math.floor(Math.random() * levelOne.length)
+  while (levelOne[randomIndex] === 1) {
+    randomIndex = Math.floor(Math.random() * levelOne.length)
+  }
+  tiles[randomIndex].classList.add('dot')
+}
+for(let i = 0; i< 4; i++) setDots()
+
+
+
+document.addEventListener('keyup', pacMove)
+
+
+
+
+//
